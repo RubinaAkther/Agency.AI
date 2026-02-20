d8da08f2d1b5934a80e212c1dacfb9b6132226b0
Rubina Akther <rubinaakther3454@gmail.com>
2026-02-20 23:52:50 +0600

    feat: complete team section ui implementation

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import TrustedBy from './components/TrustedBy';
 import Services from './components/Services';
 import OurWork from './components/OurWork';
+import Teams from './components/Teams';
 
 const App = () => {
   const [theme, setTheme] = useState(
@@ -16,6 +17,7 @@ const App = () => {
       <TrustedBy/>
       <Services/>
       <OurWork/>
+      <Teams/>
     </div>
   );
 };
